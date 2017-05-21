817e22ddb2e6ee0bdcaf09cd71091c5668257a20
Viktor Matrai <viktor0matrai@gmail.com>
2017-05-21 17:35:32 +0200

    final

--- actions.py
+++ actions.py
@@ -8,18 +8,18 @@ def display_nicknames(connect):
     query.printing_query("""SELECT nick_name FROM mentors WHERE city='Miskolc';""", connect)
 
 def carols_data(connect):
-    query.printing_query("""SELECT concat(first_name, ' ', last_name),
+    query.printing_query("""SELECT CONCAT(first_name, ' ', last_name),
         phone_number FROM applicants WHERE first_name='CAROL';""", connect)
 
 def hat_owner(connect):
-    query.printing_query("""SELECT concat(first_name ,' ', last_name), 
+    query.printing_query("""SELECT CONCAT(first_name ,' ', last_name),
         phone_number FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';""", connect)
 
 def update_jemima(connect):
     query.modification_query("""UPDATE applicants SET phone_number='003670/223-7459'
-        WHERE first_name='Jamima' and last_name='Foreman';""", connect)
+        WHERE first_name='Jemima' and last_name='Foreman';""", connect)
     query.printing_query("""SELECT first_name, last_name, phone_number FROM applicants
-        WHERE first_name='Jamima' and last_name='Foreman';""", connect)
+        WHERE first_name='Jemima' and last_name='Foreman';""", connect)
 
 def delete_arsenio_and_friend(connect):
     query.modification_query("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""", connect)
