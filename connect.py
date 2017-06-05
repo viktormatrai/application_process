954d594f73b2073cf38441a1a9265f33fad68233
Viktor Matrai <viktor0matrai@gmail.com>
2017-06-05 11:59:06 +0200

    querys deleted made templates for html docs and static folder for css

--- connect.py
+++ connect.py
@@ -1,9 +1,11 @@
 import psycopg2
 
+
 def connect_db():
     connect = psycopg2.connect("dbname='matraiv' user='matraiv' host='localhost' password='06pv24'")
-    connect.autocommit = True           
+    connect.autocommit = True
     return connect
+
 
 def commands(query, connect):
     cursor = connect.cursor()
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from flask import Flask, render_template
 import querys
 import connect
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return render_template('home_page.html')
+
+
+if __name__ == "__main__":
+    app.run()
